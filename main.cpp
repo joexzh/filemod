0edb3995b01547de55e4ca9e5f1b55d59f53c152
Joe Tse <xiezhenhao@gmail.com>
2025-02-28 14:18:04 +0800

    cli parser: switch from cli11 to Boost::program_options
    1. the utf8every inside cli11 make it difficult to convert output back to system code page on Windows
    2. more flexible and easier control using program_options over cli11

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 //
 // Created by Joe Tse on 11/26/23.
 //
-#include <CLI/CLI.hpp>
-#include <cstdlib>
+#include <boost/program_options.hpp>
 #include <exception>
 #include <filemod/modder.hpp>
 #include <iostream>
 #include <limits>
+#include <sstream>
 #include <string>
 #include <vector>
 
-static inline bool is_set(int64_t id) {
+namespace po = boost::program_options;
+
+static bool is_set(int64_t id) {
   return id != std::numeric_limits<int64_t>::min();
 }
 
-static inline bool is_set(const std::vector<int64_t> &ids) {
-  return !ids.empty();
-}
+static bool is_set(const std::vector<int64_t> &ids) { return !ids.empty(); }
 
-static inline bool is_set(const std::string &dir) { return !dir.empty(); }
+static bool is_set(const std::string &dir) { return !dir.empty(); }
 
-static inline void move_to_retbase(filemod::result<int64_t> &&from,
-                                   filemod::result_base &to) {
+static void move_to_retbase(filemod::result<int64_t> &&from,
+                            filemod::result_base &to) {
   to.success = from.success;
   if (from.success) {
     to.msg = std::to_string(from.data);
@@ -30,169 +30,234 @@ static inline void move_to_retbase(filemod::result<int64_t> &&from,
   }
 }
 
-static inline int run(int argc, char **argv) {
-  CLI::App app{"filemod is a file replacement manager."};
-  app.set_help_all_flag("--help-all");
+static void parse_subcmd(const po::options_description &desc,
+                         const po::basic_parsed_options<char> &parsed,
+                         po::variables_map &vm) {
+  auto opts = po::collect_unrecognized(parsed.options, po::include_positional);
+  opts.erase(opts.begin());
 
-  class MyFormatter : public CLI::Formatter {
-   public:
-    MyFormatter() : Formatter() {}
+  // parse again
+  po::store(po::command_line_parser(opts).options(desc).run(), vm);
+  po::notify(vm);
+}
 
-    std::string make_option_opts(const CLI::Option *) const override {
-      return "";
-    }
-  };
+static void parse_error(const po::options_description &desc,
+                        std::ostream &ostream, filemod::result_base &ret) {
+  ostream << desc;
+  ret.success = false;
+}
 
-  auto fmt = std::make_shared<MyFormatter>();
-  app.formatter(fmt);
+static void parse_add(filemod::result_base &ret, std::ostringstream &oss,
+                      po::basic_parsed_options<char> &parsed,
+                      po::variables_map &vm, std::string &tdir,
+                      std::string &mdir, int64_t &id) {
+  po::options_description desc(
+      "add target or mod\n"
+      "Usage: filemod add --tdir <target_dir>\n"
+      "       filemod add -t <target_id> --mdir <mod_dir>\n"
+      "Options");
+  desc.add_options()("tdir", po::value<std::string>(&tdir), "target directory")(
+      "tid,t", po::value<int64_t>(&id), "target id")(
+      "mdir", po::value<std::string>(&mdir), "mod source files directory")(
+      "help,h", "");
+  parse_subcmd(desc, parsed, vm);
+  filemod::modder md;
 
-  auto name_validator = [](const std::string &str) -> std::string {
-    if (str.empty() || str[0] == '-' ||
-        std::string_view(str).substr(0, 2) == "--") {
-      const char prefix[]{"invalid str: "};
-      std::string msg;
-      msg.reserve(sizeof(prefix) - 1 + str.size());
-      msg += prefix;
-      msg += str;
-      return msg;
-    }
-    return {};
-  };
-
-  app.require_subcommand(1);
-
-  int64_t tar_id{std::numeric_limits<int64_t>::min()};
-  std::vector<int64_t> tar_ids;
-  std::vector<int64_t> mod_ids;
-  std::string dir;
-
-  auto add = app.add_subcommand("add", "add target or mod to management");
-  add->add_option("--tdir", dir, "target directory")
-      ->take_last()
-      ->check(CLI::ExistingDirectory);
-  auto mdir_opt =
-      add->add_option("--mdir", dir, "mod directory")->excludes("--tdir");
-  add->add_option("-t", tar_id, "target id")
-      ->excludes("--tdir")
-      ->needs("--mdir")
-      ->take_last()
-      ->check(name_validator);
-  mdir_opt->needs("-t")->take_last()->check(CLI::ExistingDirectory);
-
-  auto ins = app.add_subcommand("install", "install mod to target directory");
-  ins->add_option("-t", tar_id, "target id")
-      ->take_last()
-      ->check(name_validator);
-  ins->add_option("--mdir", dir, "mod directory")
-      ->needs("-t")
-      ->take_last()
-      ->check(CLI::ExistingDirectory);
-  ins->add_option("-m", mod_ids, "mod ids")
-      ->excludes("-t", "--mdir")
-      ->take_all()
-      ->check(name_validator);
-
-  auto uns =
-      app.add_subcommand("uninstall", "uninstall mod from target directory");
-  uns->add_option("-t", tar_id, "target id")
-      ->take_last()
-      ->check(name_validator);
-  uns->add_option("-m", mod_ids, "mod ids")
-      ->excludes("-t")
-      ->take_all()
-      ->check(name_validator);
-
-  auto rmv =
-      app.add_subcommand("remove", "remove target or mod from management");
-  rmv->add_option("-t", tar_id, "target id")
-      ->take_last()
-      ->check(name_validator);
-  rmv->add_option("-m", mod_ids, "mod ids")
-      ->excludes("-t")
-      ->take_all()
-      ->check(name_validator)
-      ->take_last();
-
-  auto lst = app.add_subcommand("list", "list managed targets and mods");
-  lst->add_option("-t", tar_ids, "target ids")
-      ->take_all()
-      ->check(name_validator);
-  lst->add_option("-m", mod_ids, "mod ids")
-      ->excludes("-t")
-      ->take_all()
-      ->check(name_validator);
-
-  filemod::result_base ret{.success = true};
-
-  // inside callbacks dir is utf8 encoded
-
-  add->callback([&]() {
-    filemod::modder modder;
-    if (is_set(tar_id) && is_set(dir)) {  // add mod
-      move_to_retbase(modder.add_mod(tar_id, filemod::utf8str_to_path(dir)),
-                      ret);
-    } else if (is_set(dir)) {  // add target
-      move_to_retbase(modder.add_target(filemod::utf8str_to_path(dir)), ret);
-    }
-  });
-
-  rmv->callback([&]() {
-    filemod::modder modder;
-    if (is_set(mod_ids)) {  // remove mods
-      ret = modder.remove_mods(mod_ids);
-    } else if (is_set(tar_id)) {  // remove target
-      ret = modder.remove_from_target_id(tar_id);
-    }
-  });
-
-  ins->callback([&]() {
-    filemod::modder modder;
-    if (is_set(mod_ids)) {  // install mods
-      ret = modder.install_mods(mod_ids);
-    } else if (is_set(tar_id) &&
-               is_set(dir)) {  // add and install mod directly from mod dir
-      move_to_retbase(
-          modder.install_from_mod_src(tar_id, filemod::utf8str_to_path(dir)),
-          ret);
-    } else if (is_set(tar_id)) {  // install mods from target id
-      ret = modder.install_from_target_id(tar_id);
-    }
-  });
-
-  uns->callback([&]() {
-    filemod::modder modder;
-    if (is_set(mod_ids)) {  // uninstall mods
-      ret = modder.uninstall_mods(mod_ids);
-    } else if (is_set(tar_id)) {  // uninstall mod from target id
-      ret = modder.uninstall_from_target_id(tar_id);
-    }
-  });
-
-  lst->callback([&]() {
-    filemod::modder modder;
-    if (is_set(mod_ids)) {  // list mods
-      ret.msg = modder.list_mods(mod_ids);
-    } else {  // list targets
-      ret.msg = modder.list_targets(tar_ids);
-    }
-  });
-
-  argv = app.ensure_utf8(argv);
-  CLI11_PARSE(app, argc, argv);
-
-  if (!ret.success) {
-    std::cerr << ret.msg << '\n';
-    return EXIT_FAILURE;
+  if (vm.count("help")) {
+    oss << desc;
+  } else if (is_set(tdir)) {  // add target
+    move_to_retbase(md.add_target(tdir), ret);
+  } else if (is_set(mdir) && is_set(id)) {  // add mod to existing target
+    move_to_retbase(md.add_mod(id, mdir), ret);
+  } else {
+    parse_error(desc, oss, ret);
   }
-  std::cout << ret.msg << '\n';
-  return EXIT_SUCCESS;
+}
+
+static void parse_install(filemod::result_base &ret, std::ostringstream &oss,
+                          po::basic_parsed_options<char> &parsed,
+                          po::variables_map &vm, std::string &mdir, int64_t &id,
+                          std::vector<int64_t> &ids) {
+  po::options_description desc(
+      "install mod(s)\n"
+      "Usage: filemod install -t <target_id>\n"
+      "       filemod install -t <target_id> --mdir <mod_dir>\n"
+      "       filemod install -m <mod_id1>...\n"
+      "Options");
+  desc.add_options()("tid,t", po::value<int64_t>(&id), "target id")(
+      "mdir", po::value<std::string>(&mdir), "mod source directory")(
+      "mid,m", po::value<std::vector<int64_t>>(&ids)->multitoken(), "mod ids")(
+      "help,h", "");
+  parse_subcmd(desc, parsed, vm);
+  filemod::modder md;
+
+  if (vm.count("help")) {
+    oss << desc;
+  } else if (is_set(mdir) && is_set(id)) {  // install from mod source dir
+    move_to_retbase(md.install_from_mod_src(id, mdir), ret);
+  } else if (is_set(id)) {  // install all mods of a target
+    ret = md.install_from_target_id(id);
+  } else if (is_set(ids)) {  // install multiple mods
+    ret = md.install_mods(ids);
+  } else {
+    parse_error(desc, oss, ret);
+  }
+}
+
+static void parse_uninstall(filemod::result_base &ret, std::ostringstream &oss,
+                            po::basic_parsed_options<char> &parsed,
+                            po::variables_map &vm, int64_t &id,
+                            std::vector<int64_t> &ids) {
+  po::options_description desc(
+      "uninstall mod(s)\n"
+      "Usage: filemod uninstall -t <target_id>\n"
+      "       filemod uninstall -m <mod_id1>...\n"
+      "Options");
+  desc.add_options()("tid,t", po::value<int64_t>(&id), "target id")(
+      "mid,m", po::value<std::vector<int64_t>>(&ids)->multitoken(), "mod ids")(
+      "help,h", "");
+  parse_subcmd(desc, parsed, vm);
+  filemod::modder md;
+
+  if (vm.count("help")) {
+    oss << desc;
+  } else if (is_set(id)) {  // uninstall all mods of a target
+    ret = md.uninstall_from_target_id(id);
+  } else if (is_set(ids)) {  // uninstall multiple mods
+    ret = md.uninstall_mods(ids);
+  } else {
+    parse_error(desc, oss, ret);
+  }
+}
+
+static void parse_remove(filemod::result_base &ret, std::ostringstream &oss,
+                         po::basic_parsed_options<char> &parsed,
+                         po::variables_map &vm, int64_t &id,
+                         std::vector<int64_t> &ids) {
+  po::options_description desc(
+      "remove target or mod(s)\n"
+      "Usage: filemod remove -t <target_id>\n"
+      "       filemod remove -m <mod_id1>...\n"
+      "Options");
+  desc.add_options()("tid,t", po::value<int64_t>(&id), "target id")(
+      "mid,m", po::value<std::vector<int64_t>>(&ids)->multitoken(), "mod ids")(
+      "help,h", "");
+  parse_subcmd(desc, parsed, vm);
+  filemod::modder md;
+
+  if (vm.count("help")) {
+    oss << desc;
+  } else if (is_set(id)) {  // remove mod from a target
+    ret = md.remove_target(id);
+  } else if (is_set(ids)) {  // remove multiple mods
+    ret = md.remove_mods(ids);
+  } else {
+    parse_error(desc, oss, ret);
+  }
+}
+
+static void parse_list(filemod::result_base &ret, std::ostringstream &oss,
+                       po::basic_parsed_options<char> &parsed,
+                       po::variables_map &vm, std::vector<int64_t> &ids) {
+  po::options_description list_desc(
+      "display target(s) and mod(s) in database\n"
+      "Usage: filemod list [-t <target_id1>...]\n"
+      "       filemod list -m <mod_id1>...\n"
+      "Options");
+  list_desc.add_options()(
+      "tid,t", po::value<std::vector<int64_t>>(&ids)->multitoken(),
+      "target ids")("mid,m", po::value<std::vector<int64_t>>()->multitoken(),
+                    "mod ids")("help,h", "");
+  parse_subcmd(list_desc, parsed, vm);
+  filemod::modder md;
+
+  if (vm.count("help")) {
+    oss << list_desc;
+  } else if (vm.count("mid")) {  // list mods
+    ret.msg = md.list_mods(vm["mid"].as<std::vector<int64_t>>());
+  } else {  // list targets
+    ret.msg = md.list_targets(ids);
+  }
+}
+
+int parse(int argc, char *argv[]) {
+  filemod::result_base ret{.success = true};
+  std::ostringstream oss;
+
+  po::options_description global_desc(
+      "filemod is a file replacement manager.\n"
+      "Usage: filemod <command> [<args>]\n"
+      "Commands: add | install | uninstall | remove | list\n"
+      "Global Options");
+  global_desc.add_options()("help,h", "")("version,v", "");
+
+  po::options_description hidden("command");
+  hidden.add_options()("command", po::value<std::string>(), "")(
+      "subargs", po::value<std::vector<std::string>>(), "");
+
+  po::options_description global_cmd_desc;
+  global_cmd_desc.add(global_desc).add(hidden);
+
+  po::positional_options_description subcmd;
+  subcmd.add("command", 1).add("subargs", -1);
+
+  auto parsed = po::command_line_parser(argc, argv)
+                    .options(global_cmd_desc)
+                    .positional(subcmd)
+                    .allow_unregistered()
+                    .run();
+  po::variables_map vm;
+  po::store(parsed, vm);
+  po::notify(vm);
+
+  if (vm.count("command")) {
+    auto cmd = vm["command"].as<std::string>();
+
+    int64_t id = std::numeric_limits<int64_t>::min();
+    std::string tdir;
+    std::string mdir;
+    std::vector<int64_t> ids;
+
+    if ("add" == cmd) {
+      parse_add(ret, oss, parsed, vm, tdir, mdir, id);
+    } else if ("install" == cmd) {
+      parse_install(ret, oss, parsed, vm, mdir, id, ids);
+    } else if ("uninstall" == cmd) {
+      parse_uninstall(ret, oss, parsed, vm, id, ids);
+    } else if ("remove" == cmd) {
+      parse_remove(ret, oss, parsed, vm, id, ids);
+    } else if ("list" == cmd) {
+      parse_list(ret, oss, parsed, vm, ids);
+    } else {
+      parse_error(global_desc, oss, ret);
+    }
+  } else if (vm.count("help")) {
+    oss << global_desc;
+  } else if (vm.count("version")) {
+#ifdef FILEMOD_VERSION
+#define STRINGIFY(x) STRINGIFY_IMPL(x)
+#define STRINGIFY_IMPL(x) #x
+    ret.msg = STRINGIFY(FILEMOD_VERSION);
+#else
+    ret.msg = "no version information"
+#endif
+  } else {
+    parse_error(global_desc, oss, ret);
+  }
+
+  if (ret.success) {
+    std::cout << ret.msg << oss.str() << '\n';
+    return 0;
+  }
+  std::cerr << ret.msg << oss.str() << '\n';
+  return 1;
 }
 
 int main(int argc, char **argv) {
   try {
-    return run(argc, argv);
+    return parse(argc, argv);
   } catch (std::exception &e) {
     std::cerr << e.what() << '\n';
-    return EXIT_FAILURE;
+    return 1;
   }
 }
